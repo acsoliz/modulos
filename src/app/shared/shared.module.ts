0906d414a7d9598a91f07ad43ae7b81590384c5c
acsoliz <acsoliz@outlook.com>
2023-11-27 19:33:57 +0100

    shared modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { RouterModule } from '@angular/router'
+
+
+@NgModule({
+  declarations: [
+    NavigationComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavigationComponent,
+    FooterComponent
+    
+    
+  ]
+})
+export class SharedModule { }
