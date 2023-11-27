a059cabf1adbe633d8d2b2a69f9149ce16468b1e
acsoliz <acsoliz@outlook.com>
2023-11-27 18:32:37 +0100

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { RouterModule, Routes } from '@angular/router'
 
 
 const routes : Routes = [
+  {
+    path: '',
+    loadChildren: ()=> import('./welcome/welcome.module').then(m=>m.WelcomeModule)
+  },
+  {
+    path: 'courses',
+    loadChildren: ()=> import('./courses/courses.module').then(m=>m.CoursesModule)
+  }
 
 ];
 
